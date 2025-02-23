7fcf0a8066fa008d2f8f7020571298c0e8f72826
JC <144067507+lifetimingwhisper@users.noreply.github.com>
2025-02-23 13:45:24 +0000

    wee5(LV2) - [GET] 取得教練詳細資訊：{url}/api/coaches/:coachId : 1. 新增從資料庫取得教練詳細資訊 API，包含關聯至 USER table 取得 user name

--- week5/routes/coach.js
+++ week5/routes/coach.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { dataSource } = require('../db/data-source')
 const validation = require('../utils/validation')
+const logger = require('../utils/logger')('Coach')
 
 router.get('', async (req, res, next) => {
     const per = req.query.per
@@ -42,6 +43,57 @@ router.get('', async (req, res, next) => {
         logger.error(error)
         next(error)
     }
+})
+
+router.get('/:coachId', async (req, res, next) => {
+    const coachId = req.params.coachId
+    if (validation.isUndefined(coachId) || validation.isNotValidSting(coachId)) {
+      logger.warn('欄位未填寫正確')
+      res.status(400).json({
+        status: 'failed',
+        message: '欄位未填寫正確'
+      })
+      return
+    }
+
+    try {
+        const coachRepo = dataSource.getRepository('Coach')
+        const existingCoach = await coachRepo.findOne({
+            relations: ['User'],
+            where: {id : coachId}
+        })
+
+        if (!existingCoach) {
+            logger.warn('找不到該教練')
+            res.status(400).json({
+                status: 'failed',
+                message: '找不到該教練'
+            })
+            return
+        }
+
+        res.status(200).json({
+            "status" : "success",
+            "data": {
+                "user": {
+                    "name": existingCoach.User.name,
+                    "role": "COACH"
+                },
+                "coach": {
+                    "id": existingCoach.id,
+                    "user_id": existingCoach.user_id,
+                    "experience_years": existingCoach.experience_years,
+                    "description": existingCoach.description,
+                    "profile_image_url": existingCoach.profile_image_url,
+                    "created_at": existingCoach.created_at,
+                    "updated_at": existingCoach.updated_at
+                }
+            }
+        })
+    } catch(error) {
+        logger.error(error)
+        next(error)
+    } 
 })
 
 module.exports = router

1c687dc3a478dc42dc0815c6c4237e75e39916bc
JC <144067507+lifetimingwhisper@users.noreply.github.com>
2025-02-24 12:27:49 +0000

    clone week5 as the initial revision of week6

---
+++ week6/db/data-source.js
@@ -0,0 +1,29 @@
+const { DataSource } = require('typeorm')
+const config = require('../config/index')
+
+const CreditPackage = require('../entities/CreditPackages')
+const Skill = require('../entities/Skill')
+const Coach = require('../entities/Coach')
+const Course = require('../entities/Course')
+const User = require('../entities/User') 
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: config.get('db.host'),
+  port: config.get('db.port'),
+  username: config.get('db.username'),
+  password: config.get('db.password'),
+  database: config.get('db.database'),
+  synchronize: config.get('db.synchronize'),
+  poolSize: 10,
+  entities: [
+    CreditPackage,
+    Skill,
+    Coach,
+    Course,
+    User
+  ],
+  ssl: config.get('db.ssl')
+})
+
+module.exports = { dataSource }
---
+++ week6/routes/creditPackage.js
@@ -0,0 +1,103 @@
+const express = require('express')
+
+const router = express.Router()
+const { dataSource } = require('../db/data-source')
+const logger = require('../utils/logger')('CreditPackage')
+
+function isUndefined (value) {
+  return value === undefined
+}
+
+function isNotValidSting (value) {
+  return typeof value !== 'string' || value.trim().length === 0 || value === ''
+}
+
+function isNotValidInteger (value) {
+  return typeof value !== 'number' || value < 0 || value % 1 !== 0
+}
+
+router.get('/', async (req, res, next) => {
+  try {
+    const creditPackage = await dataSource.getRepository('CreditPackage').find({
+      select: ['id', 'name', 'credit_amount', 'price']
+    })
+    res.status(200).json({
+      status: 'success',
+      data: creditPackage
+    })
+  } catch (error) {
+    logger.error(error)
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, credit_amount: creditAmount, price } = req.body
+    if (isUndefined(name) || isNotValidSting(name) ||
+      isUndefined(creditAmount) || isNotValidInteger(creditAmount) ||
+            isUndefined(price) || isNotValidInteger(price)) {
+      res.status(400).json({
+        status: 'failed',
+        message: '欄位未填寫正確'
+      })
+      return
+    }
+    const creditPurchaseRepo = await dataSource.getRepository('CreditPackage')
+    const existCreditPurchase = await creditPurchaseRepo.find({
+      where: {
+        name
+      }
+    })
+    if (existCreditPurchase.length > 0) {
+      res.status(409).json({
+        status: 'failed',
+        message: '資料重複'
+      })
+      return
+    }
+    const newCreditPurchase = await creditPurchaseRepo.create({
+      name,
+      credit_amount: creditAmount,
+      price
+    })
+    const result = await creditPurchaseRepo.save(newCreditPurchase)
+    res.status(200).json({
+      status: 'success',
+      data: result
+    })
+  } catch (error) {
+    logger.error(error)
+    next(error)
+  }
+})
+
+router.delete('/:creditPackageId', async (req, res, next) => {
+  try {
+    const { creditPackageId } = req.params
+    if (isUndefined(creditPackageId) || isNotValidSting(creditPackageId)) {
+      res.status(400).json({
+        status: 'failed',
+        message: '欄位未填寫正確'
+      })
+      return
+    }
+    const result = await dataSource.getRepository('CreditPackage').delete(creditPackageId)
+    if (result.affected === 0) {
+      res.status(400).json({
+        status: 'failed',
+        message: 'ID錯誤'
+      })
+      return
+    }
+    res.status(200).json({
+      status: 'success',
+      data: result
+    })
+  } catch (error) {
+    logger.error(error)
+    next(error)
+  }
+})
+
+module.exports = router
